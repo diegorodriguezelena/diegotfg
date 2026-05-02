bb54db9892ecfc4410545bdc0019f1d804c983c0
diegorodriguezelena <145709065+diegorodriguezelena@users.noreply.github.com>
2026-05-02 18:48:25 +0200

    Add files via upload

---
+++ PROYECTO_TFG.R
@@ -0,0 +1,11 @@
+
+(library(haven))
+(datos <- ES_TFG_DAN)
+(names(datos))
+
+install.packages("shiny")
+install.packages("dplyr")
+install.packages("stringr")
+install.packages("tidyr")
+install.packages("visNetwork")
+install.packages("tibble")
